200264232b87ff8fa711601f28e0d552693931d9
Alexandre Roitman <roitmanalexandre@gmail.com>
2022-08-11 21:50:12 -0300

    feat: redirecionamento para pagina details

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {DataGrid} from '@mui/x-data-grid';
+import { useNavigate } from "react-router-dom";
 
 const columns = [
   { field: 'name', headerName: 'BREED NAME', width: 670},
@@ -7,6 +8,7 @@ const columns = [
 ];
 
 const DataTable = () => {
+  const navigate = useNavigate();
 
   const [tableData, setTableData] = useState([])
 
@@ -21,7 +23,8 @@ useEffect(() => {
       <DataGrid 
         rows={tableData}
         columns={columns}
-        pageSize={25}    
+        pageSize={25} 
+        onRowClick={() => {navigate('/details')}}   
       />
     </div>
   )
